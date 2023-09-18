5b1d1122ee7072005ddf295778a4eb436a6eb3c1
vedkm <vedkailash1@gmail.com>
2023-09-18 11:33:47 -0400

    [ADD] foliage_fixer_app: added user and manager security groups

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,21 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "foliage_fixer_app",
+    'name': "Foliage Fixer",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       An app for automatically detecting disease in tomato plants.""",
 
     'description': """
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Ved Mahadeo",
+    'website': "https://vedmahadeo.pythonanywhere.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Services/foliage_fixer',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -24,7 +23,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/foliage_fixer_security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
@@ -32,4 +32,8 @@
     'demo': [
         'demo/demo.xml',
     ],
+
+    'application': True,
+
+    'sequence': -100
 }
